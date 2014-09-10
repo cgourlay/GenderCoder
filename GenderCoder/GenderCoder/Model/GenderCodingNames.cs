419e3a348fa8026747086fcc440495672b8adbdc
cgourlay <colin.gourlay5@gmail.com>
2014-09-10 20:50:28 +0100

    Updated the project file properties.

--- GenderCoder/GenderCoder/Model/GenderCodingNames.cs
+++ GenderCoder/GenderCoder/Model/GenderCodingNames.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-using GenderCoder.Properties;
+using ColinGourlay.GenderEncoder.Properties;
 using GenderCoder.Utilities;
 
 namespace GenderCoder.Model
--- GenderCoder/GenderCoder/Properties/Resources.Designer.cs
+++ GenderCoder/GenderCoder/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GenderCoder.Properties {
+namespace ColinGourlay.GenderEncoder.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GenderCoder.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GenderCoder.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ColinGourlay.GenderEncoder.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
