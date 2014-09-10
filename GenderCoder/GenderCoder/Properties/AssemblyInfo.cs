cba9ae1bbcba14e772e5833e2d0a34ee8a92afc2
cgourlay <colin.gourlay5@gmail.com>
2014-09-10 16:34:29 +0100

    Updated the AssemblyInfo

--- GenderCoder/GenderCoder/Properties/AssemblyInfo.cs
+++ GenderCoder/GenderCoder/Properties/AssemblyInfo.cs
@@ -1,35 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GenderCoder")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("GenderCoder")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("TODO: Title goes here.")]
+[assembly: AssemblyDescription("TODO: Description goes here.")]
+[assembly: AssemblyConfiguration("TODO: Configuration goes here.")]
+[assembly: AssemblyCompany("TODO: Company goes here.")]
+[assembly: AssemblyProduct("TODO: Product goes here.")]
+[assembly: AssemblyCopyright("Copyright © TODO: Copyright goes here 2014")]
+[assembly: AssemblyTrademark("TODO: Trademark goes here.")]
+[assembly: AssemblyCulture("TODO: Culture goes here.")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9b80bf90-a952-46d7-a92a-14e15d588ea9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
