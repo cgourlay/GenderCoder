83731f8291cdff9d0b2ff00361084cb678215b16
cgourlay <colin.gourlay5@gmail.com>
2014-09-10 12:29:56 +0100

    Removed redundant code.
    Removed unused using clauses.

--- GenderCoder/GenderCoder/Entities/GenderCodingResult.cs
+++ GenderCoder/GenderCoder/Entities/GenderCodingResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenderCoder.Entities
+﻿namespace GenderCoder.Entities
 {
     public class GenderCodingResult
     {
